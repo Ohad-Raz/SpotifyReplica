9b5021e6373096b8fe6aaa6decde60a9048afe85
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-03-03 15:42:34 +0200

    base files set

---
+++ Server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use("/api/v1/users");
+// app.use('/api/v1/')
+
+module.exports = app;
