ca4aded97ee036b027b574d781b1777e48d52069
Ohad-Raz <ohadraz12@gmail.com>
2024-03-04 10:14:27 +0200

    song instead of track, artist controoler.

---
+++ Server/models/artist.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const artistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  biography: String,
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  albums: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Album"
+  }],
+  songs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Song"
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Artist = mongoose.model("Artist", artistSchema);
+
+module.exports = Artist;
