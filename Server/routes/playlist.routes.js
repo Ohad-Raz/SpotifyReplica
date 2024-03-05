5fe99b9e8a9f6b002fc03fbabecf9fea501f7899
Ohad-Raz <ohadraz12@gmail.com>
2024-03-05 14:05:39 +0200

    playlists working

--- Server/routes/playlist.routes.js
+++ Server/routes/playlist.routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const playlistController = require('../controllers/playlist.controller');
+const { auth, authorize } = require("../middlewares/auth");
 
-router.post('/', playlistController.createPlaylist);
+
+
+router.post('/',auth , playlistController.createPlaylist);
 router.get('/', playlistController.getAllPlaylists);
 router.get('/:id', playlistController.getPlaylistById);
 router.post('/:playlistId/add-song', playlistController.addSongToPlaylist);
 router.delete('/:playlistId/remove-song', playlistController.removeSongFromPlaylist);
+router.delete('/:id', auth, playlistController.deletePlaylist);
 
 module.exports = router;
