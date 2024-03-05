1bc98fca4743fb3aa257443f7e36c16ca5251525
nirglus <nirgluskin@gmail.com>
2024-03-05 12:57:10 +0200

    fix add new album

--- Server/controllers/albumController.js
+++ Server/controllers/albumController.js
@@ -1,127 +1,48 @@
 const { Album } = require("../models/album.model");
-const { uploadToCloudinary } = require("../cloudiniryFloder/cloudinary");
 
-const getAlbums = async (req, res) => {
-  try {
-    const query = req.query;
-    const albums = await Album.find({ ...query });
-    res.send({ status: "Got albums succesfully", data: albums });
-  } catch (error) {
-    res.status(400).send("Error");
-  }
-};
+const getAlbums = async(req, res) =>{
+    try {
+        const query = req.query;
+        const albums =  await Album.find({ ...query });
+        res.send({status: "Got albums succesfully", data: albums});
+    } catch (error) {
+        res.status(400).send("Error");
+    }
+}
 
-const getAlbums = async (req, res) => {
-  try {
-    const query = req.query;
-    const albums = await Album.find({ ...query });
-    res.send({ status: "Got albums succesfully", data: albums });
-  } catch (error) {
-    res.status(400).send("Error");
-  }
-};
+const getSingleAlbum = async(req, res) =>{
+    const {id} = req.params;
+    try {
+        const singleAlbum = await Album.findById(id);
+        if(singleAlbum) return res.send(singleAlbum);
+        res.send("Couldn't find album");
+    } catch (error) {
+        res.status(400).send({error: error.message});
+    }
+}
 
-const getSingleAlbum = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const singleAlbum = await Album.findById(id);
-    if (singleAlbum) return res.send(singleAlbum);
-    res.send("Couldn't find album");
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-};
+const addNewAlbum = async(req, res) =>{
+    const {title, release_date, genre, artist_id, songs} = req.body;
+    try {
+        const newAlbum = new Album({title, release_date, genre, artist_id, songs});
+        newAlbum.id = newAlbum._id;
+        await newAlbum.save();
+        res.send("Album added!", newAlbum)
+        
+    } catch (error) {
+        res.status(400).send({error: error.message});
+    }
+}
 
-const addNewAlbum = async (req, res) => {
-  const { title, release_date, genre, artist_id, songs } = req.body;
-  try {
-    const newAlbum = new Album({
-      title,
-      release_date,
-      genre,
-      artist_id,
-      songs,
-    });
-    newAlbum.id = newAlbum._id;
-    await newAlbum.save();
-    res.send("Album added!", newAlbum);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
+const deleteAlbum = async (req, res)=>{
+    const { id } = req.params;
+    try {
+        await Album.findByIdAndDelete(id);
+        return res.send("Succesfully deleted album");
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Error");
+    }
 };
 
-const deleteAlbum = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Album.findByIdAndDelete(id);
-    return res.send("Succesfully deleted album");
-  } catch (error) {
-    console.log(error);
-    res.status(400).send("Error");
-  }
-};
-
-const getSingleAlbum = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const singleAlbum = await Album.findById(id);
-    if (singleAlbum) return res.send(singleAlbum);
-    res.send("Couldn't find album");
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-};
-
-const addNewAlbum = async (req, res) => {
-  const { title, release_date, genre, artist_id, songs } = req.body;
-  try {
-    const newAlbum = new Album({
-      title,
-      release_date,
-      genre,
-      artist_id,
-      songs,
-    });
-    newAlbum.id = newAlbum._id;
-    await newAlbum.save();
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-};
-
-const deleteAlbum = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Album.findByIdAndDelete(id);
-    return res.send("Succesfully deleted album");
-  } catch (error) {
-    console.log(error);
-    res.status(400).send("Error");
-  }
-};
-const uploadPicture = async (req, res) => {
-  try {
-    const data = await uploadToCloudinary(req.file.path, "post-images");
-    await Album.updateOne(
-      { _id: req.params.id },
-      {
-        $set: {
-          imageUrl: data.url,
-          publicId: data.public_id,
-        },
-      }
-    );
-    res.status(200).send("user image uploaded with success!");
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(error);
-  }
-};
-
-module.exports = {
-  getAlbums,
-  getSingleAlbum,
-  addNewAlbum,
-  deleteAlbum,
-  uploadPicture,
-};
+module.exports = { getAlbums, getSingleAlbum, addNewAlbum,  deleteAlbum };
