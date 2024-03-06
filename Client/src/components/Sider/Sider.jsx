c34e054f12ad75e720caff78cc153dd526df22d0
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-03-06 10:54:06 +0200

    css plalist and serach set

--- Client/src/components/Sider/Sider.jsx
+++ Client/src/components/Sider/Sider.jsx
@@ -1,17 +1,46 @@
+import React, { useState, useEffect } from "react";
 import styles from "./Sider.module.css";
 import { Link } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
+import { FaHome, FaSearch } from "react-icons/fa"; // Import the search icon
 import { IoArrowBackSharp } from "react-icons/io5";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsCollection } from "react-icons/bs";
 import { MdCancel } from "react-icons/md";
-import { FaList } from "react-icons/fa6";
+import { FaList } from "react-icons/fa";
 
 import MiniPlaylist from "../MiniPlaylist/MiniPlaylist";
 
 import playlists from "../../dummy-data/playlist";
+
 export default function Sider() {
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const toggleInput = () => {
+    setShowInput(!showInput);
+    if (!showInput) {
+      setInputValue(""); // Reset input value when showing the input field
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log(inputValue);
+  }, [inputValue]);
+
+  const handleSearch = () => {
+    console.log("Search input:", inputValue);
+    // Perform search-related operations here
+  };
+
+  // Filter playlists based on input value
+  const filteredPlaylists = playlists.filter((playlist) => {
+    return playlist.name.toLowerCase().includes(inputValue.toLowerCase());
+  });
+
   return (
     <aside className={styles.aside}>
       <div className={styles.asideHeader}>
@@ -19,7 +48,6 @@ export default function Sider() {
           <Link to="/">
             <p>Home</p>
           </Link>
-
           <FaHome />
         </div>
         <div className={styles.searchHeader}>
@@ -27,8 +55,7 @@ export default function Sider() {
             {" "}
             <p>Search</p>
           </Link>
-
-          <FaSearch />
+          <FaSearch onClick={toggleInput} />
         </div>
       </div>
       <div className={styles.discover}>
@@ -48,15 +75,33 @@ export default function Sider() {
           <p>Spotify</p>
           <p>Recent</p>
         </div>
+
         <div className={styles.discoverSearch}>
           <div>
             <FaList />
             <p>Recent</p>
           </div>
-          <FaSearch />
+          <div
+            className={`${styles.searchPlaylist} ${
+              showInput && styles.activeSearch
+            }`}
+          >
+            {showInput && (
+              <div className={styles.searchInput}>
+                <input
+                  type="text"
+                  placeholder="Enter your search"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                />
+                <button onClick={handleSearch} />
+              </div>
+            )}
+            <FaSearch onClick={toggleInput} />{" "}
+          </div>
         </div>
         <div className={styles.playLists}>
-          {playlists.map((playlist) => {
+          {filteredPlaylists.map((playlist) => {
             return <MiniPlaylist key={playlist.id} playlist={playlist} />;
           })}
         </div>
