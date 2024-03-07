d93be5607614d0a14ec4df4e2cd52c682670c88a
urielbang <uri32655@gmail.com>
2024-03-07 14:17:35 +0200

    uriel

---
+++ Client/src/components/artistCardHome/ArtistCardHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import { FaCirclePlay } from "react-icons/fa6";
+
+export default function ArtistCardHome({ artist }) {
+  return (
+    <div className="cardArtistHome">
+      <img src={artist.imageUrl} alt="Artist" />
+      <p>{artist.name}</p>
+      <FaCirclePlay className="playIcon" />
+    </div>
+  );
+}
